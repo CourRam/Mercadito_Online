e95fed662bff1d134cbc5f503a80e182c89e126a
jozep <josephesguapoa@gmail.com>
2025-10-29 20:27:57 -0600

    Inicio de proyecto dependencias y propiedades

---
+++ src/main/java/com/UACH/Mercadito_Online/MercaditoOnlineApplication.java
@@ -0,0 +1,13 @@
+package com.UACH.Mercadito_Online;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MercaditoOnlineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MercaditoOnlineApplication.class, args);
+	}
+
+}
